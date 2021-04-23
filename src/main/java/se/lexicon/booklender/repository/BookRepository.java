6b48e46bbd5c6fd88759f9a5b675243656b1741e
Mikael Aurell <aurell.mikael@gmail.com>
2021-04-23 11:51:48 +0200

    added Additionally functionality to BookRepository + BookRepositoryTest

--- src/main/java/se/lexicon/booklender/repository/BookRepository.java
+++ src/main/java/se/lexicon/booklender/repository/BookRepository.java
@@ -3,5 +3,10 @@ package se.lexicon.booklender.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.booklender.entity.Book;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Integer> {
+    List<Book> findBooksByReserved(boolean reserved);
+    List<Book> findBooksByAvailable(boolean available);
+    List<Book> findBooksByTitle(String title);
 }
--- src/test/java/se/lexicon/booklender/repository/BookRepositoryTest.java
+++ src/test/java/se/lexicon/booklender/repository/BookRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import se.lexicon.booklender.entity.Book;
 import se.lexicon.booklender.repository.BookRepository;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ public class BookRepositoryTest {
 
     BookRepository bookRepository;
     Book testBook;
+    Book testBook2;
 
     @Autowired
     public void setBookRepository(BookRepository bookRepository) {
@@ -27,14 +29,23 @@ public class BookRepositoryTest {
     @BeforeEach
     public void setup(){
         testBook = new Book();
-        testBook.setTitle("How to Become a senor Java Fullstack Developer");
+        testBook.setTitle("How to Become a senor Fullstack Developer");
         testBook.setAvailable(true);
         testBook.setReserved(false);
         testBook.setMaxLoanDays(30);
         testBook.setFinePerDay(BigDecimal.valueOf(1 / 8));
         testBook.setDescription("Java");
 
+        testBook2 = new Book();
+        testBook2.setTitle("How to Become a senor Fullstack Developer");
+        testBook2.setAvailable(true);
+        testBook2.setReserved(true);
+        testBook2.setMaxLoanDays(30);
+        testBook2.setFinePerDay(BigDecimal.valueOf(1 / 8));
+        testBook2.setDescription("C#");
+
         bookRepository.save(testBook);
+        bookRepository.save(testBook2);
     }
 
     @Test
@@ -42,7 +53,7 @@ public class BookRepositoryTest {
     public void test_save(){
         List<Book> bookList = new ArrayList<>();
         bookRepository.findAll().iterator().forEachRemaining(bookList::add);
-        Assertions.assertEquals(30,bookList.get(0).getMaxLoanDays());
+        assertEquals(30,bookList.get(0).getMaxLoanDays());
     }
 
     @Test
@@ -50,7 +61,7 @@ public class BookRepositoryTest {
     public void test_find_by_id(){
         List<Book> bookList = new ArrayList<>();
         bookRepository.findAll().iterator().forEachRemaining(bookList::add);
-        Assertions.assertEquals(bookList.get(0).getBookId(), bookRepository.findById(1).get().getBookId());
+        assertEquals(bookList.get(0).getBookId(), bookRepository.findById(1).get().getBookId());
     }
 
     @Test
@@ -59,6 +70,33 @@ public class BookRepositoryTest {
         bookRepository.delete(testBook);
         List<Book> bookList = new ArrayList<>();
         bookRepository.findAll().iterator().forEachRemaining(bookList::add);
-        Assertions.assertEquals(0, bookList.size());
+        assertEquals(0, bookList.size());
     }
+
+    @Test
+    @DisplayName("Test4 Find By Reserved Status")
+    public void test_find_by_reserved_status(){
+        List<Book> actual = bookRepository.findBooksByReserved(true);
+        assertEquals("How to Become a senor Fullstack Developer", actual.get(0).getTitle());
+        testBook.setReserved(true);
+        bookRepository.save(testBook);
+        actual = bookRepository.findBooksByReserved(true);
+        assertEquals("How to Become a senor Fullstack Developer", actual.get(1).getTitle());
+    }
+
+    @Test
+    @DisplayName("Test5 Find By Available Status")
+    public void test_find_by_available_status(){
+        assertEquals("C#", bookRepository.findBooksByAvailable(true).get(1).getDescription());
+    }
+
+    @Test
+    @DisplayName("Test6 Find By Title")
+    public void test_find_by_title(){
+        assertEquals("Java", bookRepository
+                .findBooksByTitle("How to Become a senor Fullstack Developer").get(0).getDescription());
+        assertEquals("C#", bookRepository
+                .findBooksByTitle("How to Become a senor Fullstack Developer").get(1).getDescription());
+    }
+
 }
